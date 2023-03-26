00a10a66dac18350d517184f49e083beb5f2a794
cold-pumpkin <pch4623@gmail.com>
2023-03-26 14:47:51 +0900

    UI Challenge - Buttons Section

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       backgroundColor: Color(0xff181818),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 40),
-        child: Column(children: [
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           SizedBox(
             height: 80,
           ),
@@ -34,6 +34,50 @@ class MyApp extends StatelessWidget {
                       fontSize: 18,
                     )),
               ]),
+            ],
+          ),
+          SizedBox(
+            height: 120,
+          ),
+          Text(
+            'Total Balance',
+            style:
+                TextStyle(fontSize: 22, color: Colors.white.withOpacity(0.8)),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            '\$ 5 194 482',
+            style: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(45),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 50,
+                  ),
+                  child: Text(
+                    'Transfer',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              )
             ],
           )
         ]),
